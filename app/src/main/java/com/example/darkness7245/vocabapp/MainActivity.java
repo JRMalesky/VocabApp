8c953d967916f4622de3bf5050a71e8147cda957
Sean Ermish <SMErmish@fullsail.edu>
2017-05-08 22:44:08 -0400

    scrambled word working

--- app/src/main/java/com/example/darkness7245/vocabapp/MainActivity.java
+++ app/src/main/java/com/example/darkness7245/vocabapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.darkness7245.vocabapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public int randomdificulty()
+    {
+        Random rand = new Random();
+        return rand.nextInt(3);
+    }
+    public String ScrambleWord(String _string)
+    {
+        Random rand = new Random();
+        char a[] = _string.toCharArray();
+
+        for( int i=0 ; i< a.length ; i++ )
+        {
+            int j = rand.nextInt(a.length);
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        return new String( a );
+    }
+
 }
